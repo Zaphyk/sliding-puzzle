9b1f1423558771330ca3834ed318d13ebeafa01b
Zaphyk <zaphyk@live.com>
2017-12-04 14:49:25 -0300

    added background + partial cell movement

--- puzzle.js
+++ puzzle.js
@@ -7,12 +7,14 @@ var imageSizeZ = 500;
 
 //Generate the pieces because doing 16 different styles is too much.
 function generatePuzzlePieces(){
-	var baseGroup = $(".puzzle-group");
+	var baseGroup = $("#group");
 
 	for(x = 0; x < sizeX; x++){
 		for(z = 0; z < sizeZ; z++){
 			if(x * 4 + z < puzzleSize-1)
-			baseGroup.append("<img id='piece-"+ (x*sizeX + z)+"' class='puzzle-item' onclick='puzzleClick();' >");
+			baseGroup.append("<img id='piece-"+ (x*sizeX + z)+"' class='puzzle-item' onclick='puzzleClick("+(x*sizeX + z)+");' >");
+
+			setPuzzlePosition((x*sizeX + z), (x*sizeX + z));
 		}
 	}
 }
@@ -29,9 +31,48 @@ function generatePuzzlePiecesStyle(){
 }
 
 function shuffle(){
+	var optionsArray;
 
+	for(x = 0; x < sizeX; x++){
+		for(z = 0; z < sizeZ; z++){
+			
+		}
+	}
 }
 
-function puzzleClick(){
+function setPuzzlePosition(cellIndex, indexPosition){
+
+	$("#piece-"+cellIndex).css("top",  Math.floor(indexPosition / sizeX) * (imageSizeZ / sizeZ) );
+	$("#piece-"+cellIndex).css("left", (indexPosition % sizeZ) * (imageSizeX / sizeX) );
+	$("#piece-"+cellIndex).attr("cell", indexPosition);
+}
+
+function cellOccupied(x, z){
+	for(i = 0; i < puzzleSize; i++){
+		if( $("#piece-"+i).attr("cell") == sizeX*x+z)
+			return true;
+	}
+	return false;
+}
+
+function puzzleClick(index){
+	console.log(index);
+	//decompose the index
+	var x = Math.floor(index / sizeX);
+	var z = Math.floor(index % sizeZ);
+
+	//check for empty neighbours, maybe do a for?
+
+	if( x+1 < sizeX && !cellOccupied(x+1,z+0) )
+		setPuzzlePosition(index, (x+1) * sizeX + z+0);
+
+	if( z+1 < sizeZ && !cellOccupied(x+0,z+1) )
+		setPuzzlePosition(index, (x+0) * sizeX + z+1);
+
+	if( x-1 > 0 && !cellOccupied(x-1,z+0) )
+		setPuzzlePosition(index, (x-1) * sizeX + z+0);
+
+	if( z-1 > 0 && !cellOccupied(x+0,z-1) )
+		setPuzzlePosition(index, (x+0) * sizeX + z-1);
 
 }
